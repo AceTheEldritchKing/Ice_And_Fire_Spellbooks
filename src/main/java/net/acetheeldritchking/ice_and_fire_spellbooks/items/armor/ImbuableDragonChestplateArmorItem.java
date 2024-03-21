5fc17d149bafc647ee595e27dc4471f692a2aaf3
Ace The King <solcat0414@gmail.com>
2024-03-21 13:34:54 -0400

    Imbuable Armor but it only works in new worlds?

--- src/main/java/net/acetheeldritchking/ice_and_fire_spellbooks/items/armor/ImbuableDragonChestplateArmorItem.java
+++ src/main/java/net/acetheeldritchking/ice_and_fire_spellbooks/items/armor/ImbuableDragonChestplateArmorItem.java
@@ -3,25 +3,21 @@ package net.acetheeldritchking.ice_and_fire_spellbooks.items.armor;
 import io.redspace.ironsspellbooks.api.spells.IPresetSpellContainer;
 import io.redspace.ironsspellbooks.api.spells.ISpellContainer;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 
 public abstract class ImbuableDragonChestplateArmorItem extends DragonArmorItem implements IPresetSpellContainer {
-    public ImbuableDragonChestplateArmorItem(IFSArmorMaterial materialIn, EquipmentSlot slot, Properties settings) {
-        super(materialIn, slot, settings);
+    public ImbuableDragonChestplateArmorItem(IFSArmorMaterial material, EquipmentSlot slot, Properties settings) {
+        super(material, slot, settings);
     }
 
     @Override
     public void initializeSpellContainer(ItemStack itemStack) {
-        if (itemStack == null)
-        {
+        if (itemStack == null) {
             return;
         }
 
-        if (itemStack.getItem() instanceof ArmorItem armorItem && armorItem.getSlot() == EquipmentSlot.CHEST)
-        {
-            if (!ISpellContainer.isSpellContainer(itemStack))
-            {
+        if (itemStack.getItem() instanceof DragonArmorItem armorItem && armorItem.getSlot() == EquipmentSlot.CHEST) {
+            if (!ISpellContainer.isSpellContainer(itemStack)) {
                 var spellContainer = ISpellContainer.create(1, true, true);
                 spellContainer.save(itemStack);
             }
